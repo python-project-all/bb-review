1fcfec29f2dca68345ee2653e673f1fd3de08edd
joonsol <71648638+joonsol@users.noreply.github.com>
2026-05-03 21:10:14 +0900

    Read secrets from environment variables

--- app/config.py
+++ app/config.py
@@ -28,3 +28,9 @@ def get_secret(
         return default_value
 
     raise EnvironmentError(f"Set the {key} environment variable.")
+
+
+MONGO_DB_NAME = get_secret("MONGO_DB_NAME")
+MONGO_URL = get_secret("MONGO_URL")
+NAVER_API_ID = get_secret("NAVER_API_ID")
+NAVER_API_SECRET = get_secret("NAVER_API_SECRET")
